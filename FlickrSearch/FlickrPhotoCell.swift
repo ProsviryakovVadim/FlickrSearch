55d18d6817274957b8f588bf193e4b5742413f3a
Vadim <vgprosviryakov@gmail.com>
2017-07-20 12:55:47 +0300

    Start project

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Vadim on 7/19/17.
+//  Copyright © 2017 Vadim Prosviryakov. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
